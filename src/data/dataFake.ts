eb5427c319af3eb7584e594c5b14b9edbc5a0eea
Leonardograut <omgleo@live.com>
2023-09-03 21:24:06 -0300

    final project

---
+++ src/data/dataFake.ts
@@ -0,0 +1,47 @@
+export const dataFake = [
+ {
+
+
+    "id":"1",
+    
+    "title": " Já está disponível nas principais plataformas de compra e aluguel de filmes o novo longa da Mistério S/A, uma animação que promove o encontro de dois dos cachorros mais famosos do mundo dos desenhos animados: Scooby-Doo e Coragem, o Cão Covarde",
+    "description":"",
+    "photoCover":"https://www.otempo.com.br/image/contentid/policy:1.2543020:1631889554/Screen-Shot-2021-09-17-at-11-30-13-AM-png.png?f=3x2&w=1224"
+ },
+
+ {
+
+
+    "id":"2",
+    
+    "title": "Um clássico dos anos 90 do Cartoon Network, Coragem  o Cão Covarde, aki Estao os 7 viloes : Veterinário Cruel, Katz,Mad Dog ,Mulher Perfeccionista, Patinho, A Rainha da Poça Negra,Fred , Mosca Mordomo",
+    
+    "description":"",
+    "photoCover":"https://i.ytimg.com/vi/r3TaJkLGeuk/maxresdefault.jpg"
+ },
+
+ {
+
+
+    "id":"3",
+   
+    "title": "Ele fez sucesso no final dos anos 90 e se tornou sensação para crianças e até mesmo adultos, mas a história por trás dele pode ser bem assustadora! ",
+    "description":"",
+    "photoCover":"https://pm1.aminoapps.com/7408/920fdec2e92c8739ec8e5f5fd26a9641bea16654r1-500-281v2_hq.jpg"
+ },
+
+ {
+
+
+   "id":"4",
+  
+   "title": "Coragem, o Cão Covarde, clássico do Cartoon Network, pode ganhar uma nova série. Segundo John R. Dilworth, criador da animação, o novo programa se passaria antes do original e seria uma “prequel”.",
+   "description":"",
+   "photoCover":"https://imgsapp2.correiobraziliense.com.br/app/noticia_127983242361/2018/10/26/715051/20181025164646706457u.jpg"
+}
+
+
+
+
+]
+ 
